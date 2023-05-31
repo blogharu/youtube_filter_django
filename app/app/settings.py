06b01923506cdd0e122e8f9191195fc91cf60484
blogharu <blogharu@gmail.com>
2023-05-30 19:15:53 -0500

    add app called youtube_filter

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'youtube_filter.apps.YoutubeFilterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
